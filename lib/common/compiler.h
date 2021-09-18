3b1a5f56ebf59f93379abb7a40056f858aa2c03b
Steven Noonan <steven@uplinklabs.net>
2021-09-18 01:42:28 -0700

    compiler.h: use __attribute__((unused)) on clang-cl
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- lib/common/compiler.h
+++ lib/common/compiler.h
@@ -73,7 +73,7 @@
 #endif
 
 /* UNUSED_ATTR tells the compiler it is okay if the function is unused. */
-#if defined(__GNUC__)
+#if defined(__GNUC__) || defined(__clang__)
 #  define UNUSED_ATTR __attribute__((unused))
 #else
 #  define UNUSED_ATTR
